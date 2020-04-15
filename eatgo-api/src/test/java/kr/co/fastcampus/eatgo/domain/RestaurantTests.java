a6337164918dae1360ed0b97eea44746d7c4e17b
문동현 <moon@moon.local>
2020-04-16 00:06:21 +0900

    first commit

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,24 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip","");
+        assertEquals(restaurant.getName(), "Bob zip");
+        assertEquals(restaurant.getAddress(),"Seoul");
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+
+        assertEquals(restaurant.getInformation(), "Bob zip in Seoul");
+    }
+}
