9366e408709c34bffd8e8952d55e4978faf161ed
문동현 <moon@moon.local>
2020-04-17 01:36:15 +0900

    2020.04.16 Restaurant List & Detail

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,7 +1,10 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import org.apache.catalina.util.ErrorPageSupport;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,13 +13,20 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
-    @GetMapping("/restaurant")
-    public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
+    private RestaurantRepository repository = new RestaurantRepository();
 
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
-        restaurants.add(restaurant);
+    @GetMapping("/restaurants")
+    public List<Restaurant> list() {
+
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        Restaurant restaurant = repository.findById(id);
+
+        return restaurant;
     }
 }
