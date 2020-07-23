b14f8d1ac7d5035fd44d6e4874a9f7e2c9025be0
문동현 <moon@moon.local>
2020-07-23 14:18:03 +0900

    2020.07.23 commit

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -1,10 +1,13 @@
 package kr.co.fastcampus.eatgo.domain;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
 
     private final long id;
     private final String name;
     private final String address;
+    private List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
     public Restaurant(Long id,String name,String address) {
         this.id = id;
@@ -28,5 +31,17 @@ public class Restaurant {
         return address;
     }
 
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
 
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        for (MenuItem menuItem : menuItems) {
+            addMenuItem(menuItem);
+        }
+    }
 }
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,9 +1,16 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 
+import kr.co.fastcampus.eatgo.application.RestaurantService;
+import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
+import kr.co.fastcampus.eatgo.domain.MenuItemRepositoryImpl;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.StringContains.containsString;
@@ -16,6 +23,16 @@ public class RestaurantControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+
+    @SpyBean(RestaurantService.class)
+    private RestaurantService restaurantService;
+
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepository restaurantRepository;
+
+    @SpyBean(MenuItemRepositoryImpl.class)
+    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
@@ -30,7 +47,8 @@ public class RestaurantControllerTest {
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1004")))
-                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")))
+                .andExpect(content().string(containsString("Kimchi")));
 
         mvc.perform(get("/restaurants/2020"))
                 .andExpect(status().isOk())
